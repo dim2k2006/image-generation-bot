7020c9398ccc909615ed1a29f0bfb9bc54126f56
Dmitry Malofeev <dim2k2006@yandex.ru>
2025-02-10 09:18:02 +0100

    Remove unused code

--- src/domain/user/index.ts
+++ src/domain/user/index.ts
@@ -1,6 +1,5 @@
 export { User } from './user.model';
 export { UserRepository } from './user.repository';
-export { default as UserRepositorySupabase } from './user.repository.supabase';
 export { default as UserRepositoryInMemory } from './user.repository.in-memory';
 export { UserService, CreateUserInput } from './user.service';
 export { default as UserServiceImpl } from './user.service.impl';
