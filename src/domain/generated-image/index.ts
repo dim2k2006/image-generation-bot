11a03e6dc58636e44fa5ed9809a3b01d499f0227
Dmitry Malofeev <dim2k2006@yandex.ru>
2025-02-10 09:07:35 +0100

    Add index.ts

---
+++ src/domain/generated-image/index.ts
@@ -0,0 +1,4 @@
+export { GeneratedImage } from './generated-image.model';
+export { GeneratedImageRepository } from './generated-image.repository';
+export { GeneratedImageService, CreateImageInput } from './generated-image.service';
+export { default as GeneratedImageServiceImpl } from './generated-image.service.impl';
