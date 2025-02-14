272493b149801c231147e4ecb754114c97935210
Dmitry Malofeev <dim2k2006@yandex.ru>
2025-02-14 08:24:46 +0100

    Update ChatProvider

--- src/providers/chat/chat.provider.ts
+++ src/providers/chat/chat.provider.ts
@@ -1,3 +1,9 @@
 export interface ChatProvider {
   sendMessages(chatId: string, messages: string[]): Promise<void>;
+  sendPhotos(chatId: string, photos: Photo[]): Promise<void>;
 }
+
+export type Photo = {
+  url: string;
+  caption?: string;
+};
